6b5d1e13d09117daf682ff6502d26a66005dace8
Vital Boisset <vital.boisset@gmail.com>
2020-03-23 15:40:37 -0400

    3 passing tests

--- index.js
+++ index.js
@@ -35,9 +35,13 @@ function addNumbers(num1, num2) {
  * the returned value should look like: 'Goodbye, Andy. Have a great day.'
  * 
 */
-function sayGoodbye(/* code here */) {
+function sayGoodbye(/* code here */name) {
   /* code here */
+  return ('Goodbye, ' + name + '. Have a great day.');
+  
 }
+
+
 
 /**
  * ### Challenge `temperatureCtoF`
@@ -53,8 +57,9 @@ function sayGoodbye(/* code here */) {
  * Hint 1: The formula for converting celsius to fahrenheit is t*9/5 + 32 where t is the temperature in celsius.
  * Hint 2: There is a very easy way to round numbers in JS. Do a google search to find out how. 
 */
-function temperatureCtoF(/* code here */) {
+function temperatureCtoF(/* code here */temperatureCtoF) {
   /* code here */
+  return Math.round(temperatureCtoF*9/5+32);
 }
 
 /**
